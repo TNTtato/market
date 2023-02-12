3e0105aa612598c4c84b81d12c489de1083c7b14
Gustavo <penarandagustavo5@gmail.com>
2023-02-12 10:46:19 -0500

    create Category entity

---
+++ src/main/java/com/gustavo/market/persistence/entity/Category.java
@@ -0,0 +1,22 @@
+package com.gustavo.market.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category")
+    private Long idCategory;
+
+    private String description;
+    private Boolean state;
+}
