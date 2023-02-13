b769c7a7dce4a649990c9ff099dbe3d57bb07c01
Gustavo <penarandagustavo5@gmail.com>
2023-02-13 15:32:25 -0500

    two query methods

--- src/main/java/com/gustavo/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/gustavo/market/persistence/crud/ProductCrudRepository.java
@@ -3,5 +3,10 @@ package com.gustavo.market.persistence.crud;
 import com.gustavo.market.persistence.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends JpaRepository<Product, Long> {
+    List<Product> findByIdCategoryOrderByNameAsc(Long idCategory);
+    Optional<List<Product>> findByInStockLessThanAndState(int inStock, boolean state);
 }
