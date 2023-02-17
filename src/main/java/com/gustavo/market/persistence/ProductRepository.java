69438b97326535d3d73808380f71da347af2ff3d
Gustavo <penarandagustavo5@gmail.com>
2023-02-17 10:59:28 -0500

    migrating to domain based and inject dependencies

--- src/main/java/com/gustavo/market/persistence/ProductRepository.java
+++ src/main/java/com/gustavo/market/persistence/ProductRepository.java
@@ -1,35 +1,60 @@
 package com.gustavo.market.persistence;
 
+import com.gustavo.market.domain.ProductDomain;
+import com.gustavo.market.domain.repository.ProductDomainRepository;
 import com.gustavo.market.persistence.crud.ProductCrudRepository;
 import com.gustavo.market.persistence.entity.Product;
+import com.gustavo.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements ProductDomainRepository {
+
     private ProductCrudRepository productCrudRepository;
+    private ProductMapper productMapper;
 
-    public List<Product> getAll() {
-        return productCrudRepository.findAll();
-    }
-    public List<Product> getByCategoryId(long idCategory) {
-        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
-    }
-
-    public Optional<List<Product>> getScarce(int inStock) {
-        return productCrudRepository.findByInStockLessThanAndState(inStock, true);
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    public ProductRepository(ProductCrudRepository productCrudRepository, ProductMapper productMapper) {
+        this.productCrudRepository = productCrudRepository;
+        this.productMapper = productMapper;
     }
 
-    public Optional<Product> getProduct(long idProduct) {
-        return productCrudRepository.findById(idProduct);
+    @Override
+    public List<ProductDomain> getAll() {
+
+        List<Product> products = productCrudRepository.findAll();
+        return productMapper.toProductsDomain(products);
     }
 
-    public Product save(Product product) {
-        return productCrudRepository.save(product);
+    @Override
+    public List<ProductDomain> getByCategoryId(long idCategory) {
+        List<Product> products = productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+        return productMapper.toProductsDomain(products);
     }
 
+    @Override
+    public Optional<List<ProductDomain>> getScarce(int inStock) {
+        Optional<List<Product>> products = productCrudRepository.findByInStockLessThanAndState(inStock, true);
+        return products.map( prod -> productMapper.toProductsDomain((prod)));
+    }
+
+    @Override
+    public Optional<ProductDomain> getProduct(long idProduct) {
+        return productCrudRepository.findById(idProduct).map(product -> productMapper.toProductDomain(product));
+    }
+
+    @Override
+    public ProductDomain save(ProductDomain productDomain) {
+        Product product = productMapper.toProduct(productDomain);
+        return productMapper.toProductDomain(productCrudRepository.save(product));
+    }
+
+    @Override
     public void delete(long idProduct) {
         productCrudRepository.deleteById(idProduct);
     }
